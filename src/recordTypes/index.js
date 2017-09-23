6b40af6e7d12416f3ca3493db0b8337d43fbb995
Cesar Villalobos <cesarv.h@berkeley.edu>
2017-09-23 16:45:48 -0700

    BAMPFA-551: Customized condition check record

--- src/recordTypes/index.js
+++ src/recordTypes/index.js
@@ -2,10 +2,12 @@ import group from './group';
 import intake from './intake';
 import valuation from './valuation';
 import collectionobject from './collectionobject';
+import conditioncheck from './conditioncheck';
 
 export default pluginContext => ({
   group: group(pluginContext),
   intake: intake(pluginContext),
   collectionobject: collectionobject(pluginContext),
   valuation: valuation(pluginContext),
+  conditioncheck: conditioncheck(pluginContext),
 });
