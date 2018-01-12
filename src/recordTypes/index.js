06930a6303324ab5a38b31337fb0c7106b0fb57f
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-01-11 16:40:57 -0800

    BAMPFA-563: Removed changes in work authority

--- src/recordTypes/index.js
+++ src/recordTypes/index.js
@@ -9,8 +9,6 @@ import media from './media';
 import movement from './movement';
 import organization from './organization';
 import person from './person';
-import place from './place';
-import work from './work';
 
 export default pluginContext => [
   citation,
@@ -23,8 +21,6 @@ export default pluginContext => [
   movement,
   organization,
   person,
-  place,
-  work,
 ]
 .map(configurer => configurer(pluginContext))
 .reduce(merge, {
